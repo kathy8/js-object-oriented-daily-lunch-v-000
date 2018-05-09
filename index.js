2124b56ddfe3fb3046dcc2b03fa4aebfb33a4542
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-05-09 06:37:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 // global datastore
-let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+let store = { neighborhoods: [], meals: [], customers: [], deliveries: [],
+employers: []};
+
+let customerId = 0
+let mealId = 0
+let deliveryId = 0
+let employerId = 0
+
+function removeDuplicates(arr, key){
+  let unique_array = []
+  for(let i = 0;i < arr.length; i++){
+    if(unique_array.indexOf(arr[i])== -1){
+      unique_array.push(arr[i])
+    }
+  }
+  return unique_array
+}
